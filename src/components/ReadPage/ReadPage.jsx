7974175204c10b33a323b6c93d339c1f9f6e9f94
Essenbay Assel <esenbay.asel@mail.ru>
2022-11-16 08:29:22 +0600

    modified:   src/components/ReadPage/ReadPage.jsx

--- src/components/ReadPage/ReadPage.jsx
+++ src/components/ReadPage/ReadPage.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import './ReadPage.css'
+import '../../storage/'
 
 function ReadPage({openItem}){
-
+    const targetPath = `../../storage/${}`
 }
 export default ReadPage
