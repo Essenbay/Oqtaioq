55d0bb61716137a249796622a3cc46c943f9d694
T1m4ay <tero4y@gmail.com>
2022-11-16 08:27:24 +0600

    modified:   src/ListPage.test.js

--- src/ListPage.test.js
+++ src/ListPage.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import ListPage from './pages/ListPage/ListPage';
+import { render, screen } from "@testing-library/react";
+import ListPage from "./pages/ListPage/ListPage";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<ListPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// project
