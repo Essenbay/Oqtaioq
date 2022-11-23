1b7e9c6c1f941e28cc58e1748a8c093e9bf6c308
T1m4ay <tero4y@gmail.com>
2022-11-23 09:57:00 +0600

    modified:   src/components/ListBlock/ListBlock.css 	modified:   src/components/ListBlock/ListBlock.jsx 	modified:   src/components/ListItem/ListItem.css 	modified:   src/components/ListItem/ListItem.jsx

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -3,8 +3,8 @@ import React, {useState} from 'react';
 
 function ListItem({item, toRead}){
     return(
-        <div className="list-item" onClick={()=>{toRead(item.id,1,1)}}>
-            <img src={item.image} width={250} height={250}/>
+        <div className="listItem" onClick={()=>{toRead(item.id,1,1)}}>
+            <img src={item.image} className="imagePreview" />
             <h2>{item.title}</h2>
         </div>
     )
