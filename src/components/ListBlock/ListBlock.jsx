1ace6e82c652385e0cda7a27c436f364b738c9b0
Essenbay Assel <esenbay.asel@mail.ru>
2022-11-25 17:39:37 +0600

    modified:   src/components/ListBlock/ListBlock.jsx

--- src/components/ListBlock/ListBlock.jsx
+++ src/components/ListBlock/ListBlock.jsx
@@ -5,7 +5,6 @@ import './ListBlock.css'
 function ListBlock({active, toRead, items}){
     return(
         <div className={active ? "list-block list-block-active" : "list-block"}>
-
             <ListItem item={items[0]} toRead={toRead} /> 
             <ListItem item={items[1]} toRead={toRead} />  
         </div>
